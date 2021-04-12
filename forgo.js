4f6950f060973c695df883dd5a09fee594a3e3a0
Elliott <elliott@forgo.io>
2021-04-12 00:50:51 -0700

    add jsx

--- forgo.js
+++ forgo.js
@@ -1,6 +1,24 @@
+import { h } from 'preact'
+import ReactDOMServer from 'react-dom/server'
+
+function App() {
+  return (
+    <html>
+      <head>
+        <title>Hello from JSX</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
+
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+  // renderToString generates html string from JSX components.
+  const response = new Response(ReactDOMServer.renderToString(<App />), {
+    headers: { "content-type": "text/html; charset=utf-8" },
   });
+
   event.respondWith(response);
 });
