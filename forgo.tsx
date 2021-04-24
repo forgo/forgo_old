05d91f6068ea9d74d837b789ff7ac72c66849b12
Elliott <elliott@forgo.io>
2021-04-24 16:41:44 -0700

    tilde

--- forgo.tsx
+++ forgo.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 // You need to import `h` factory function as Deno Deploy
 // uses it instead of `React.createElement`
 import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
 import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 
-import { App } from "./app.tsx";
+import { App } from "~/app.tsx";
 
 addEventListener("fetch", (event) => {
   // renderToString generates html string from JSX components.
