166756573c8cb5eed54ee8570a301c62247756a0
Elliott <elliott@forgo.io>
2021-04-24 16:54:42 -0700

    Update forgo.tsx

--- forgo.tsx
+++ forgo.tsx
@@ -1,6 +1,6 @@
 /// <reference path="./deploy.d.ts" />
 
-import React from "react";
+import React from "https://esm.sh/react@17.0.2";
 
 // You need to import `h` factory function as Deno Deploy
 // uses it instead of `React.createElement`
@@ -9,7 +9,7 @@ import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
 
 import { App } from "~/app.tsx";
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: FetchEvent) => {
   // renderToString generates html string from JSX components.
   const response = new Response(renderToString(<App />), {
     headers: { "content-type": "text/html; charset=uft-8" },
